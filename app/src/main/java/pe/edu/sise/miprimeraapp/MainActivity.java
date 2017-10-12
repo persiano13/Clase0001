8b24df2c2144c203e2ee6fa6f952fa7a0ae2c8e4
Percy A. Arellano Samamé <paas131456@gmail.com>
2017-10-11 22:25:05 -0500

    Initial commit

---
+++ app/src/main/java/pe/edu/sise/miprimeraapp/MainActivity.java
@@ -0,0 +1,14 @@
+package pe.edu.sise.miprimeraapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //concepto de polimorfismo, ondestroy,
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
